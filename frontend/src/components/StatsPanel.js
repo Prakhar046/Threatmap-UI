926fc2a36a7bc9714bd723adc673017c51b22b8d
Prakhar046 <107406442+Prakhar046@users.noreply.github.com>
2025-08-20 17:21:37 +0530

    Update StatsPanel.js

--- frontend/src/components/StatsPanel.js
+++ frontend/src/components/StatsPanel.js
@@ -71,7 +71,7 @@ const StatsPanel = ({ isSidebarOpen, toggleSidebar }) => {
   const [targetedIndustries, setTargetedIndustries] = useState([]);
  
   useEffect(() => {
-    const nationSocket = new WebSocket('ws://localhost:8000/ws/top5_country/');
+    const nationSocket = new WebSocket('ws://threatmap-backend-only.onrender.com/ws/top5_country/');
     nationSocket.onmessage = (event) => {
       const nationMessage = JSON.parse(event.data);
       const nationData = nationMessage.data;  
